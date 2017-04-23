99edc2d5929b1ae30219b99d7d65ac8426b68ae8
Osterpiegl <knollmarvin6@gmail.com>
2017-04-24 01:15:49 +0200

    Zusammenfassungsfenster

--- src/Controller/BuchungszusammenfassungController.java
+++ src/Controller/BuchungszusammenfassungController.java
@@ -1,105 +1,19 @@
 package Controller;
 
+import Model.Enums.Views;
+import Model.Klassen.MAIN;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-
 public class BuchungszusammenfassungController {
-
-    @FXML
-    private Button BestatigenButton;
-
-    @FXML
-    private Label StartortText;
-
-    @FXML
-    private Label SitzplatzText;
-
-    @FXML
-    private Label VornameText;
-
-    @FXML
-    private Label AnkunftsdatumText;
-
-    @FXML
-    private Label VornameFeld;
-
-    @FXML
-    private Label BuchungsIDFeld;
-
-    @FXML
-    private Label RuckflugFeld;
-
-    @FXML
-    private Button AndernButton;
-
-    @FXML
-    private Label StartdatumFeld;
-
-    @FXML
-    private Label PreisText;
-
-    @FXML
-    private Label StartdatumText;
-
-    @FXML
-    private Label NachnameText;
-
-    @FXML
-    private Label ZielortText;
-
-    @FXML
-    private Label BuchungsIDText;
-
-    @FXML
-    private Label PreisFeld;
-
-    @FXML
-    private Label AnkunftsdatumFeld;
-
-    @FXML
-    private Label StartortFeld;
-
-    @FXML
-    private Label SitzplatzFeld;
 
     @FXML
     private Label ZusammenfassungText;
 
     @FXML
-    private Label NachnameFeld;
-
-    @FXML
-    private Label RuckflugText;
-
-    @FXML
-    private Label ZielortFeld;
-
-    @FXML
-    private Label HinflugFeld;
-
-    @FXML
-    private Label GepackText;
-
-    @FXML
-    private Label GepackFeld;
-
-    @FXML
-    private Label HinflugText;
-
-
-    public void initialize() {
-    }
-
-
-    @FXML
-    void BestatigenAction(ActionEvent event) {
-    }
-
-    @FXML
-    void AndernAction(ActionEvent event) {
+    void ZumKundenprofilAction(ActionEvent event) {
+        MAIN.fensterOeffnen(Views.KundenProfil);
     }
 
 }
